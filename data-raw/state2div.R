58a847e6e27465f213b0784e791a516944d2e8ef
Kira McDonald <kira.mcd.99@gmail.com>
2020-10-16 18:27:47 -0400

    fixed last add

--- data-raw/state2div.R
+++ data-raw/state2div.R
@@ -1,32 +1,39 @@
 ## code to prepare `state2div` xwalk.
 
-
+library(sf)
 library(dplyr)
+rm(list=ls())
 # get all states + territories ------------------------------------------
 states = tigris::states()
 states$GEOID
 tibble(states)
 colnames(states)
 # take format i want
-state2div <- states %>%
+states <- states %>%
   tibble() %>%
   select(1:3, STUSPS, NAME)
-colnames(state2div) <- tolower(colnames(state2div))
-colnames(state2div)[4] <- "abv"
+colnames(states) <- tolower(colnames(states))
+colnames(states)[4] <- "abv"
 
+states
+states$statefp
 # add div/region names
 # Uses transcription from census bureau
 # pdf here:
 # https://github.com/cphalpert/census-regions/blob/master/us%20census%20bureau%20regions%20and%20divisions.csv
 # transcription on khealy's github https://github.com/kjhealy
 divtranscription <- read.csv("https://raw.githubusercontent.com/kjhealy/fips-codes/master/state_fips_master.csv")
+
 divtranscription <- divtranscription %>%
   select(contains("region"), contains("division")) %>%
-  mutate_all(as.character)
+  mutate_all(as.character)  %>%
+  distinct()
+  #mutate(fips = stringr::str_pad(fips, 2, "left", "0"))
 tibble(divtranscription)
 
-state2div <- state2div %>%
-  left_join(divtranscription)
+state2div <- states %>%
+  left_join(divtranscription
+            , by=c("region", "division"))
 
 state2div %>% count(division, division_name)
 state2div %>% filter(statefp > 55)
@@ -35,6 +42,11 @@ state2div %>% filter(statefp == 11)
 # name non-DC territories
 state2div$region_name <- tidyr::replace_na(state2div$region_name, "territory")
 state2div$division_name <- tidyr::replace_na(state2div$division_name, "territory")
+
+
+# arrange -----------------------------------------------------------------
+state2div <- state2div %>%
+  arrange(statefp)
 
 # write -------------------------------------------------------------------
 
