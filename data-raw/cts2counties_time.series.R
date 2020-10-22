35417b42e77534ec3a7f3303d552cb57fba23124
Kira McDonald <kira.mcd.99@gmail.com>
2020-10-22 15:59:05 -0400

    adding first entries of cts2counties across time xwalk

--- data-raw/cts2counties_time.series.R
+++ data-raw/cts2counties_time.series.R
@@ -1,47 +1,77 @@
 library(sf)
 library(dplyr)
-
+rm(list=ls())
 # https://www.census.gov/programs-surveys/geography/technical-documentation/county-changes.2010.html
 stateL = xwalks::state2div$statefp
 
-
-# get cts from 2010/2015 ; census bureau has official county correspond --------
-library(purrr)
-cts_2010 = purrr::map(as.numeric(stateL),
-                           possibly( ~tigris::tracts(state = .,
-                                          cb = T,
-                                          year = 2010)
-                                     , otherwise = NA)
-                          ) # they added more colonies since 2010 i guess? so i need the possibly
-cts_2010 <- cts_2010[!is.na(cts_2010)] %>% do.call('rbind', .)
-
-cts_2015 = purrr::map_dfr(stateL,
+# 2015 cts generated elsewhere in pkg
+#cts.2015 = divDat::cts
+cts.2015 = purrr::map_dfr(stateL,
                           ~tigris::tracts(state = .,
-                                          cb = T,
-                                          year = 2015)
-)
+                                          cb = F,
+                                          year = 2015))
+
+
+# get counties for 2010 and 2015 -----------------------------------------------
+counties.2015 = tigris::counties(year = 2015)
+counties.2010 = tigris::counties(year = 2010)
+
+
+# differentiate colnames -------------------------------------------------------
+colnames(cts.2015)[1:4] = paste0(colnames(cts.2015)[1:4], ".2015")
+colnames(cts.2015)[1:4] = tolower(colnames(cts.2015)[1:4])
+counties.2010 <- counties.2010 %>% select(1,2,3,4,5,6, geometry)
+
+
+# check method by comparing 2015 counties bundled w/ cts and generated  --------
+check.2015 = xwalks::generate.coterminous.xwalk(cts.2015, counties.2015)
+
+check.2015[,c("countyfp.2015", "COUNTYFP")]
+sum(check.2015$countyfp.2015 != check.2015$COUNTYFP)
+# check
+check.2015 <- check.2015 %>% select(1:4, NAMELSAD.y)
+# generate 2015-2010 xwalk -----------------------------------------------------
+
+ts10_15 = xwalks::generate.coterminous.xwalk(cts.2015, counties.2010)
+
+colnames(ts10_15)
+ts10_15 <- ts10_15 %>% select(1:6,13:18)
+
+ts10_15 %>% filter(countyfp.2015 != COUNTYFP10)
+# yay exactly the expected ones, as per
+# https://www.census.gov/programs-surveys/geography/technical-documentation/county-changes.2010.html
 
 
 
-# compare ----------------------------------------------------------------------
-# insane that the 2010 one is so disorganized! both state and statefp columns, smh
+cts2counties_time.series <-
+  ts10_15 %>%
+  select(
+    statefp = statefp.2015,
+    countyfp.2015,
+    counfyfp.2010 = COUNTYFP10,
+    tractce = tractce.2015,
+    namelsad10 = NAMELSAD10
+  )
 
-tibble(cts_2010)
+# add 2015 county names too; why not
+colnames(check.2015) = tolower(colnames(check.2015))
+check.2015 <- check.2015 %>% select(1:4, namelsad15 = "namelsad.y")
+check.2015$namelsad15
 
-cts_2010$GEOID.2010 = paste0(cts_2010$STATEFP,
-                        cts_2010$COUNTYFP,
-                        cts_2010$TRACT )
-cts_2015$GEOID.2015 = cts_2015$GEOID
-'sum(duplicated(cts_2010$TRACT))
-tibble(cts_2015)
+cts2counties_time.series <-
+  cts2counties_time.series %>%
+  left_join(check.2015,
+            by = c("statefp" = "statefp.2015",
+                   "countyfp.2015",
+                   "tractce" = "tractce.2015")
+            )
 
-cts_2010[! cts_2010$GEOID %in% cts_2015$GEOID,] %>% tibble() %>% count(STATEFP)
-cts_2010[! cts_2010$GEO_ID %in% cts_2015$AFFGEOID,] %>% tibble()
-'
+# rearrange cols and organize
+cts2counties_time.series <- cts2counties_time.series %>%
+  select(1,2,3,4,6,5,7)
 
-# just generate spatial overlap xwalk ------------------------------------------
 
-ten2fifteen =
-  xwalks::get.spatial.overlap(cts_2010, cts_2015,
-                              "GEOID.2010" , "GEOID.2015"
-                              )
+# write ------------------------------------------------------------------------
+
+usethis::use_data(cts2counties_time.series
+                  ,overwrite = T)
