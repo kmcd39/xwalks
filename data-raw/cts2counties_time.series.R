f3f7573509ccbdab60ab4ad0dd29d22806c9644c
Kira McDonald <kira.mcd.99@gmail.com>
2020-10-23 16:03:08 -0400

    improved ct/county time-series xwalk

--- data-raw/cts2counties_time.series.R
+++ data-raw/cts2counties_time.series.R
@@ -1,5 +1,6 @@
 library(sf)
 library(dplyr)
+library(purrr)
 rm(list=ls())
 # https://www.census.gov/programs-surveys/geography/technical-documentation/county-changes.2010.html
 stateL = xwalks::state2div$statefp
@@ -20,6 +21,7 @@ counties.2010 = tigris::counties(year = 2010)
 # differentiate colnames -------------------------------------------------------
 colnames(cts.2015)[1:4] = paste0(colnames(cts.2015)[1:4], ".2015")
 colnames(cts.2015)[1:4] = tolower(colnames(cts.2015)[1:4])
+
 counties.2010 <- counties.2010 %>% select(1,2,3,4,5,6, geometry)
 
 
@@ -71,7 +73,79 @@ cts2counties_time.series <- cts2counties_time.series %>%
   select(1,2,3,4,6,5,7)
 
 
+# also check another way ------------------------------------------------------
+
+# ct2ct xwalk
+# it seems like some tract names change as well, which is annoying, so I generate
+# ct2ct xwalk to see if this works better or can be more straightforward
+cts.2015 = purrr::map_dfr(stateL,
+                          ~tigris::tracts(state = .,
+                                          cb = T,
+                                          year = 2015))
+colnames(cts.2015)[1:4] = paste0(colnames(cts.2015)[1:4], ".2015")
+colnames(cts.2015)[1:4] = tolower(colnames(cts.2015)[1:4])
+
+cts.2010 = purrr::map(stateL,
+                        possibly( ~tigris::tracts(state = .,
+                                                  cb = T,
+                                                  year = 2010),
+                                  otherwise = NA))
+cts.2010 <- cts.2010[!is.na(cts.2010)]
+cts.2010 <- do.call('rbind', cts.2010)
+tibble(cts.2010)
+cts.2010 <-cts.2010 %>% select(state10=2, county10=3, tract10=4,
+                               NAME10 = NAME) %>%
+  mutate(geoid10 = paste0(state10, county10, tract10))
+
+tibble(cts.2015)
+cts.2015 <- cts.2015 %>% select(1:3,geoid.2015 = GEOID, NAME15 = NAME, ALAND15=ALAND)
+
+# it seems like census bureau broke out water and other special areas differently in
+# between censuses, even though census tracts aren't supposed to change...
+# from reference:
+#The Census Bureau assigned a census tract code of 9900 to represent census tracts delineated to cover
+#large bodies of water. In addition, census tract codes in the 9400s represent American Indian Areas and
+#codes in the 9800s represent special land use areas.
+#  drop those
+cts.2015 %>% arrange(ALAND15)
+cts.2015 <- cts.2015 %>% filter(ALAND15 > 0)
+# cts.2010 %>% filter(as.numeric(NAME10) >= 9800 ) %>% mapview()
+# cts.2010 %>% filter(as.numeric(NAME10) >= 9900 ) %>% mapview()
+cts.2010 <- cts.2010 %>% filter(as.numeric(NAME10) < 9900 )
+#cts.2010 <- cts.2010 %>% filter(ALAND15 > 0)
+ct2ct = xwalks::get.spatial.overlap(cts.2010, cts.2015,
+                                    "geoid10", "geoid.2015")
+
+duplicated.geods10 = ct2ct$geoid10[duplicated(ct2ct$geoid10)]
+
+sum(ct2ct$geoid10 != ct2ct$geoid.2015)
+
+tmp = ct2ct %>%
+  tibble() %>%
+  group_by(geoid10) %>%
+  filter(round(perc.area,2) ==
+           round(max(perc.area),2))# %>%
+  #ungroup() %>% group_by(geoid.2015) %>%
+  #filter(round(perc.area,2) ==
+  # round(max(perc.area),2))
+
+cts.2010[!cts.2010$geoid10 %in% tmp$geoid10, ] %>% mapview()
+# some territories and some additional "special land use areas" are new from 2010 definitions..
+cts.2015[!cts.2015$geoid.2015 %in% tmp$geoid.2015, ] %>% mapview()
+
+ct2ct <- ct2ct %>%
+  tibble() %>%
+  select(1:2) %>%
+  left_join(select(tibble(cts.2010),
+                   county10, geoid10)) %>%
+  left_join(select(tibble(cts.2015),
+                   county15 = COUNTYFP, geoid.2015)) %>%
+  arrange(geoid10)
+
+
 # write ------------------------------------------------------------------------
+
+cts2counties_time.series <- ct2ct
 
 usethis::use_data(cts2counties_time.series
                   ,overwrite = T)
