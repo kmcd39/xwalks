19a3575e9f6a005a54f7025a25d5853eeae10719
Kira McDonald <kira.mcd.99@gmail.com>
2021-02-12 09:30:05 -0500

    Changed cbg2plc xwalk; insanely they're often not coterminous

--- data-raw/plc2cz_co_and_cbg.R
+++ data-raw/plc2cz_co_and_cbg.R
@@ -28,7 +28,6 @@ plcs <-
   divM::conic.transform(plcs)
 
 # get cbgs ---------------------------------------------------------------------
-
 counties <- xwalks::co2cz$countyfp
 geo.cbgs <-
   purrr::map2_dfr(substr(counties, 1, 2),
@@ -39,26 +38,41 @@ geo.cbgs <-
                                         cb = T)
                   )
 
-# pointilize
+# transform
 geo.cbgs <-
   divM::conic.transform(geo.cbgs)
 
-geo.cbgs <-
-  st_point_on_surface(geo.cbgs)
 
 # trim colms & differntiate for after join
 geo.cbgs <- geo.cbgs %>%
   select(cbg.geoid = GEOID)
 
-# use CBGs to get place populations -----------------------------------------------
+# link cbgs 2 places -----------------------------------------------
 
-# link cbgs 2 places
-cbg.plc <- st_join(plcs,
-                   geo.cbgs)
 
-# add demographics
+plcs <- plcs %>% rename(plc.geoid = geoid)
+
+cbg.plc <- xwalks::get.spatial.overlap(
+  geo.cbgs,
+  plcs,
+  "cbg.geoid",
+  "plc.geoid"
+)
+
+
+cbg.plc$perc.area %>% summary()
+
+# add demographics ------------------------------------------------------
+cbg.plc <- cbg.plc %>%
+  arrange(cbg.geoid)
+
+cbg.plc <- cbg.plc %>%
+  tibble() %>%
+  select(1:3)
+
 abv.demos <- safegraphSeg::demos.2019 %>%
   select(1,2)
+
 cbg.plc <- cbg.plc %>%
   left_join(abv.demos,
              by = c("cbg.geoid" = "geoid"))
@@ -66,46 +80,47 @@ cbg.plc <- cbg.plc %>%
 
 # selected places -- those with min population >=100k
 sel <- tibble(cbg.plc) %>%
-  group_by(statefp, geoid, name, lsad) %>%
-  summarise(pop = sum(population))
+  group_by(plc.geoid) %>%
+  summarise(pop = sum(population * perc.area))
 
+
+# plc geoids where pop >= 100k
 sel <- sel %>%
   filter(pop >= 1e5)
 
+sel.plcs <- plcs %>%
+  filter(plc.geoid %in% sel$plc.geoid) %>%
+  rename(geoid = plc.geoid)
 
+
+sel.plcs
+
+cbg.plc
 
 # use pop-filtered plcs to generate a cbg2selectedplc xwalk --------------------
 cbg2plc <- cbg.plc %>%
   filter(geoid %in% sel$geoid)
 
-cbg2plc <- cbg2plc %>%
-  tibble() %>%
+cbg2plc <-
+  tibble(cbg2plc) %>%
   select(
     statefp,
-    plc.geoid = geoid,
-    cbg.geoid)
+    plc = plc.geoid,
+    cbg = cbg.geoid)
 
 # write
-
 # usethis::use_data(cbg2plc, overwrite=T)
 
 
 # add xwalk to county & cz ----------------------------------------------------------
-
-# add back in Place geometries
-sel <- left_join(sel,
-                 plcs[,c("geoid")])
-
-sel <- st_sf(sel) %>% divM::conic.transform()
 
 # many places ARE coterminous with counties, but not all of them
 # ( https://www2.census.gov/geo/pdfs/reference/GARM/Ch9GARM.pdf )
 
 # I make a xwalk with % overlaps, and filter only those that have <1% overlap of a
 # Place in a given CZ
-
 sel2cz_aw <- xwalks::get.spatial.overlap(
-  sel,
+  sel.plcs,
   divDat::czs_simplified,
   "geoid",
   "cz"
@@ -119,7 +134,7 @@ sel2cz_aw %>%
   arrange(geoid)
 # spot check map
 library(mapview)
-plcs %>%
+sel.plcs %>%
   filter(geoid %in% "0454050") %>%
   mapview() +
   mapview(divDat::czs %>%
@@ -131,9 +146,12 @@ plcs %>%
 
 # clean clms -------------------------------------------------------------------
 
-pop.plc2cz <- sel2cz_aw %>%
+pop.plc2cz <-
+  sel2cz_aw %>%
   tibble() %>%
-  select(plc = 1, cz = 2, 3)
+  select(plc = 1,
+         cz = 2,
+         plc2cz.perc.overlap = 3)
 
 pop.plc2cz
 
@@ -142,9 +160,7 @@ pop.plc2cz
 
 plc.co.cz <-
   pop.plc2cz %>%
-  left_join(xwalks::co2cz) %>%
-  rename(plc2cz.perc.overlap = perc.area)
-
+  left_join(xwalks::co2cz)
 
 
 # last reorg -------------------------------------------------------------------
